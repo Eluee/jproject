06161731cab26a8bab16674657d6695fc706d995
Eluee <touch1027@naver.com>
2024-10-10 16:43:17 +0900

    jpvacabulary function add

--- src/js/jpvocabulary.js
+++ src/js/jpvocabulary.js
@@ -1,3 +1,15 @@
 import axios from "axios";
 import { SERVERURL } from "./constants";
 
+// /api/jpdictionary/difflevel/{difflevel}
+async function getJpDictData(difflevel) {
+    try {
+        const response = await axios.get(`${SERVERURL}/api/jpdictionary/difflevel/${difflevel}`);
+        console.log(response.data);  // 서버로부터 받은 데이터
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+
+export default getJpDictData;
+
